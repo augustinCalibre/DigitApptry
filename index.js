22432332a30d92f84f33eaf534ea76abb8a05f37
augustin GRAH <grahaugustin@gmail.Com>
2022-07-11 11:18:01 +0000

    initial

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+const express = require('express')
+const cors = require('cors')
+const connectDB = require('./config/db')
+const bodyParser = require('body-parser')
+const routes=require('./routes/root')
+
+const port=3000;
+
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+
+app.use(routes);
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || port,()=> console.log(`Server running in  ${port}`))
+
