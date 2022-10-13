53d3cf7ff3427bf2a41604c78e6c4c1a17e37329
augustin GRAH <grahaugustin@gmail.Com>
2022-10-13 19:06:03 +0000

    delete image suffix

--- routes/life_school/life_school.js
+++ routes/life_school/life_school.js
@@ -7,8 +7,7 @@ const storage = multer.diskStorage({
       cb(null, './public/uploads')
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.originalname+ '-' + uniqueSuffix)
+      cb(null, file.originalname)
     }
   })
 
